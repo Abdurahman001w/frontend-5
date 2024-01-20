5977c6e3bc1715390f84eabbd7aaddff2eba1434
abdurahman <atger@gmail.com>
2024-01-20 17:07:59 +0300

    'frontend-5-5'

---
+++ frontend-5-5/src/components/Base.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/i.webp";
+import Detective from "./Detective";
+function Base() {
+  return (
+    <div>
+      <Detective />
+      <h1>hello world</h1>
+      <img src={logo} alt="111" />
+    </div>
+  );
+}
+export default Base;
